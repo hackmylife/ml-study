bb8ba4910903f0332b090935beefe7be3c95449c
nw <nw@linecorp.com>
2022-01-02 16:32:32 +0900

    fix bug

--- deep-learning-from-scratch-3/dezero/utils.py
+++ deep-learning-from-scratch-3/dezero/utils.py
@@ -61,6 +61,5 @@ def plot_dot_graph(output, verbose=True, to_file='graph.png'):
         f.write(dot_graph)
 
     extension = os.path.splitext(to_file)[1][1:]
-    cmd = 'dot {} -`T {} -o {}'.format(graph_path, extension, to_file)
-    print(cmd)
+    cmd = 'dot {} -T {} -o {}'.format(graph_path, extension, to_file)
     subprocess.run(cmd, shell=True)
