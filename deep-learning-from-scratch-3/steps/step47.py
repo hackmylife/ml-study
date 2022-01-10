f715ef9ec4f8954e2f03bc745563740dc3ac79b1
nw <nw@linecorp.com>
2022-01-10 22:29:36 +0900

    implement cross entropy error

--- deep-learning-from-scratch-3/steps/step47.py
+++ deep-learning-from-scratch-3/steps/step47.py
@@ -7,17 +7,12 @@ from dezero import optimizers
 import dezero.functions as F
 from dezero.models import MLP
 
-def softmax1d(x):
-    x = as_variable(x)
-    y = F.exp(x)
-    sum_y = F.sum(y)
-    return y / sum_y
-
 
 model = MLP((10,3))
 
-x = np.array([[0.2, -0.4]])
+x = np.array([[0.2, -0.4], [0.3, 0.5], [1.3, -3.2], [2.1, 0.3]])
+t = np.array([2, 0, 1, 0])
 y = model(x)
-p = F.softmax(y)
+p = F.softmax_cross_entropy(y, t)
 print(y)
 print(p)
