f1f9600633d82eaa85e5d7a0294e721e82090012
nw <nw@linecorp.com>
2022-01-12 20:52:17 +0900

    step49

---
+++ deep-learning-from-scratch-3/steps/step49.py
@@ -0,0 +1,51 @@
+import math
+
+if '__file__' in globals():
+    import os, sys
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+import numpy as np
+import dezero.functions as F
+import dezero.datasets
+from dezero.models import MLP
+from dezero import optimizers
+
+
+# hyper parameter
+max_epoch = 300
+batch_size = 30
+hidden_size = 10
+lr = 1.0
+
+# datasets class
+train_set = dezero.datasets.Spiral()
+
+# gemerate model / optimizer
+model = MLP((hidden_size, 10))
+optimizer = optimizers.SGD(lr).setup(model)
+
+data_size = len(train_set)
+max_iter = math.ceil(data_size / batch_size)
+
+for epoch in range(max_epoch):
+    index = np.random.permutation(data_size)
+    sum_loss = 0
+
+    for i in range(max_iter):
+        batch_index = index[i * batch_size:(i + 1) * batch_size]
+        batch = [train_set[i] for i in batch_index]
+        batch_x = np.array([example[0] for example in batch])
+        batch_t = np.array([example[1] for example in batch])
+
+        # calc
+        y = model(batch_x)
+        loss = F.softmax_cross_entropy(y, batch_t)
+        model.cleargrads()
+        loss.backward()
+        optimizer.update()
+
+        sum_loss += float(loss.data) * len(batch_t)
+
+    avg_loss = sum_loss / data_size
+    print('ephock %d, loss %.2f' % (epoch + 1, avg_loss))
+
+
