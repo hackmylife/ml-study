cff26bad26b789080ab91b3fe75ca9757e61573d
nw <nw@linecorp.com>
2022-01-17 21:58:16 +0900

    save/load learned wight

--- deep-learning-from-scratch-3/dezero/layers.py
+++ deep-learning-from-scratch-3/dezero/layers.py
@@ -1,3 +1,5 @@
+import os.path
+
 import numpy as np
 
 from dezero.core import Parameter
@@ -47,6 +49,40 @@ class Layer:
         for param in self.params():
             param.to_gpu()
 
+    def save_weights(self, path):
+        self.to_cpu()
+
+        params_dict = {}
+        self._flatten_params(params_dict)
+        array_dict = {key: param.data
+                      for key, param in params_dict.items()
+                      if param is not None}
+
+        try:
+            np.savez_compressed(path, **array_dict)
+        except (Exception, KeyboardInterrupt) as e:
+            if os.path.exists(path):
+                os.remove(path)
+            raise
+
+    def load_weights(self, path):
+        npz = np.load(path)
+        params_dict = {}
+        self._flatten_params(params_dict)
+        for key, param in params_dict.items():
+            param.data = npz[key]
+        
+
+    def _flatten_params(self, params_dict, parent_key=""):
+        for name in self._params:
+            obj = self.__dict__[name]
+            key = parent_key + '/' + name if parent_key else name
+
+            if isinstance(obj, Layer):
+                obj._flatten_params(params_dict, key)
+            else:
+                params_dict[key] = obj
+
 
 class Linear(Layer):
     def __init__(self, out_size, nobias=False, dtype=np.float32, in_size=None):
@@ -77,3 +113,4 @@ class Linear(Layer):
 
         y = F.linear(x, self.W, self.b)
         return y
+
