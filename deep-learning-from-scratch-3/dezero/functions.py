b9ac4d241c8db7550ff81412f90e0984c2434712
nw <nw@linecorp.com>
2022-01-22 17:27:46 +0900

    support Pooling functions

--- deep-learning-from-scratch-3/dezero/functions.py
+++ deep-learning-from-scratch-3/dezero/functions.py
@@ -403,9 +403,12 @@ def relu(x):
 # conv2d / col2im / im2col / basic_math
 # =============================================================================
 from dezero.functions_conv import conv2d_sinple
+from dezero.functions_conv import conv2d
+from dezero.functions_conv import deconv2d
+from dezero.functions_conv import pooling_simple
+from dezero.functions_conv import pooling
 from dezero.functions_conv import im2col
 from dezero.functions_conv import col2im
-from dezero.functions_conv import pooling_simple
 from dezero.core import add
 from dezero.core import sub
 from dezero.core import rsub
