2efb8a034993efe341be2fa735676196a86dcfbd
nw <nw@linecorp.com>
2022-03-21 22:05:23 +0900

    add softmax with loss layer

--- deep-learning-from-scratch-2/common/layers.py
+++ deep-learning-from-scratch-2/common/layers.py
@@ -1,4 +1,5 @@
 import numpy as np
+from functions import softmax, cross_entropy_error
 
 
 class MatMul:
@@ -58,3 +59,29 @@ class Affine:
         self.grads[1][...] = db
         return dx
 
+
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.params, self.grads = [], []
+        self.y = None
+        self.t = None
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+
+        if self.t.size == self.y.size:
+            self.t = self.t.argmax(axis=1)
+
+        loss = cross_entropy_error(self.y, self.t)
+        return loss
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+
+        dx = self.y.copy()
+        dx[np.arange(batch_size), self.t] -= 1
+        dx *= dout
+        dx = dx / batch_size
+
+        return dx
