df4897120f66679efc6d194aeb691171b6f7214b
nw <nw@linecorp.com>
2022-04-19 23:17:47 +0900

    dice simulation

---
+++ deep-learning-from-scratch-4/ch05/dice.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+def sample(dices=2):
+    x = 0
+    for _ in range(dices):
+        x += np.random.choice([1, 2, 3, 4, 5, 6])
+    return x
+
+trial = 1000
+
+samples = []
+for _ in range(trial):
+    s = sample()
+    samples.append(s)
+    V = sum(samples) / len(samples)
+    print(V)
