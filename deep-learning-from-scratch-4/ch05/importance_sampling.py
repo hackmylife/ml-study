669b877d8ea58420409ac5660d163b69f102fa7f
nw <nw@linecorp.com>
2022-04-27 00:06:28 +0900

    importance sampling model

---
+++ deep-learning-from-scratch-4/ch05/importance_sampling.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+x = np.array([1, 2, 3])
+pi = np.array([0.1, 0.1, 0.8])
+
+e = np.sum(x * pi)
+print('E_pi[x]', e)
+
+n = 100
+samples = []
+for _ in range(n):
+    s = np.random.choice(x, p=pi)
+    samples.append(s)
+
+mean = np.mean(samples)
+var = np.var(samples)
+print('MC: {:.2f} (var: {:.2f})'.format(mean, var))
