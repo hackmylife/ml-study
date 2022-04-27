267f63b981581d64c502ae0cc80133a459e155cb
nw <nw@linecorp.com>
2022-04-27 23:36:21 +0900

    importance sampling using another probability distribution

--- deep-learning-from-scratch-4/ch05/importance_sampling.py
+++ deep-learning-from-scratch-4/ch05/importance_sampling.py
@@ -7,12 +7,17 @@ pi = np.array([0.1, 0.1, 0.8])
 e = np.sum(x * pi)
 print('E_pi[x]', e)
 
+b = np.array([1/3, 1/3, 1/3])
 n = 100
 samples = []
+
 for _ in range(n):
-    s = np.random.choice(x, p=pi)
-    samples.append(s)
+    idx = np.arange(len(b))
+    i = np.random.choice(idx, p=b)
+    s = x[i]
+    rho = pi[i] / b[i]
+    samples.append(rho * s)
 
 mean = np.mean(samples)
 var = np.var(samples)
-print('MC: {:.2f} (var: {:.2f})'.format(mean, var))
+print('IS: {:.2f} (var: {:.2f})'.format(mean, var))
